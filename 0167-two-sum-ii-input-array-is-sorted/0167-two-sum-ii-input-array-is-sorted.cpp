783b06698019006a54df189a650fead7e52fc594
TEJA B <teja.battula009@gmail.com>
2026-08-28 22:59:11 +0530

    Time: 0 ms (100%), Space: 19.6 MB (33.64%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int>ans;
+         
         int n=numbers.size();
         int left=0;
         int right=n-1;
@@ -10,16 +10,14 @@ public:
 
             if(numbers[left]+numbers[right]== target)
             {
-                ans.push_back(left+1);
-                ans.push_back(right+1);
-                break;
-            }
+                 return {left+1,right+1}
+;            }
             else if(numbers[left]+numbers[right]>target)
             {
                 right--;
             }
             else left++;
         }
-        return ans;
+        return {0,0};
     }
 };
